26eb3604ae744c7d5fddd4fee596ea1b16cfaa2f
Arun Kumar <arunprajapat629@gmail.com>
2026-06-17 09:27:15 +0530

    Fix firebase crash on standalone build

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,13 +6,13 @@ import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "AIzaSyCHPn-jMxmflg2cJPccFFue8o1SpSzVyNM",
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "onway-f5999.firebaseapp.com",
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "onway-f5999",
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "onway-f5999.firebasestorage.app",
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "40420149902",
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "1:40420149902:web:8fcfcb3279f0ade03a97df",
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-8F47CPBZD8",
 };
 
 // Initialize Firebase
